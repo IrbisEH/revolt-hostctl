35bd0140f5acaa0e48c7903a040569c1495bacea
ehristenko <hristenko.evgeniy@yandex.ru>
2026-02-17 18:58:53 +0300

    add basic shelve adapter

--- src/revolt_hostctl/adapters/storage/shelve_db.py
+++ src/revolt_hostctl/adapters/storage/shelve_db.py
@@ -1,12 +1,33 @@
 import shelve
+from pathlib import Path
+from src.revolt_hostctl.app.config import STORAGE_DIR
 
 
 class ShelveAdapter:
     def __init__(self):
-        pass
+        self.data = None
+        self.storage_dir = Path(STORAGE_DIR)
+        self.storage.mkdir(parents=True, exist_ok=True)
+        self.db_file = self.storage_dir / "shelve_data.db"
 
-    def load(self):
-        pass
+    def __enter__(self):
+        if self.data is None:
+            self.data = shelve.open(self.db_file, flag="c")
+        return self
 
-    def save(self):
-        pass
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.data is not None:
+            self.data.close()
+            self.data = None
+
+    def _ensure_open(self):
+        if self.data is None:
+            raise Exception('ShelveAdapter is not opened')
+
+    def get(self, key):
+        self._ensure_open()
+        return self.data.get(key)
+
+    def set(self, key, value):
+        self._ensure_open()
+        self.data[key] = value
---
+++ src/revolt_hostctl/app/config.py
@@ -0,0 +1,3 @@
+import os
+
+STORAGE_DIR = os.path.abspath("../storage")
