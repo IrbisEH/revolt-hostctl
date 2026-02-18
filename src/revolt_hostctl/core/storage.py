f9da525af0c09c2a10798abb8500cc36a3593de2
ehristenko <hristenko.evgeniy@yandex.ru>
2026-02-18 20:38:03 +0300

    refactor storage crud methods

--- src/revolt_hostctl/core/storage.py
+++ src/revolt_hostctl/core/storage.py
@@ -2,11 +2,12 @@ from revolt_hostctl.core.models import Network, Host
 
 
 class Storage:
-    COLLECTORS = ('networks', 'hosts')
     CLASS_MAP = {'networks': Network,'hosts': Host}
+    COLLECTORS = tuple(CLASS_MAP.keys())
 
     def __init__(self, adapter):
         self.adapter = adapter
+
         for attr_key in self.COLLECTORS:
             setattr(self, attr_key, dict())
 
@@ -27,52 +28,39 @@ class Storage:
                 data = [i.to_dict() for i in getattr(self, attr_key).values()]
                 db.set(attr_key, data)
 
-    def list_hosts(self):
-        for item in self.hosts.values():
-            yield item
+    def list(self, _type):
+        self._valid(_type)
+        data = getattr(self, _type)
+        return data.values()
 
-    def get_host(self, _id=None):
-        if _id is None:
-            raise Exception('Invalid storage id')
-        return self.hosts.get(_id)
+    def get(self, _type, _id):
+        self._valid(_type)
+        data = getattr(self, _type)
+        return data.get(_id)
 
-    def add_host(self, obj=None):
-        if obj is None or not isinstance(obj, Host):
-            raise Exception('Invalid storage class')
-        self.hosts[obj._id] = obj
+    def add(self, _type, obj):
+        self._valid(_type, obj)
+        data = getattr(self, _type)
+        data[obj._id] = obj
 
-    def update_host(self, obj=None):
-        if obj is None or not isinstance(obj, Host):
-            raise Exception('Invalid storage class')
-        self.add_host(obj)
+    def update(self, _type, obj):
+        self.add(_type, obj)
 
-    def remove_host(self, _id=None):
-        if _id is None:
-            raise Exception('Invalid storage id')
-        del self.hosts[_id]
+    def remove(self, _type, _id):
+        self._valid(_type)
+        data = getattr(self, _type)
+        return data.pop(_id, None)
 
-    def list_networks(self):
-        for item in self.networks.values():
-            yield item
+    def _valid(self, _type, obj=None):
+        self._valid_type(_type)
+        if obj is not None:
+            self._valid_class(_type, obj)
 
-    def get_network(self, _id=None):
-        if _id is None:
-            raise Exception('Invalid storage id')
-        return self.networks.get(_id)
+    def _valid_type(self, _type):
+        if not isinstance(_type, str) or _type not in self.COLLECTORS:
+            raise ValueError(f"Invalid storage type: {_type}")
 
-    def add_network(self, obj=None):
-        print(obj)
-        print(type(obj))
-        if obj is None or not isinstance(obj, Network):
-            raise Exception('Invalid storage class')
-        self.networks[obj._id] = obj
-
-    def update_network(self, obj=None):
-        if obj is None or not isinstance(obj, Network):
-            raise Exception('Invalid storage class')
-        self.add_network(obj)
-
-    def remove_network(self, _id=None):
-        if _id is None:
-            raise Exception('Invalid storage id')
-        del self.networks[_id]
+    def _valid_class(self, _type, obj):
+        klass = self.CLASS_MAP[_type]
+        if not isinstance(obj, klass):
+            raise ValueError(f"Invalid storage class: {obj}")
