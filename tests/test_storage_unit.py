a106a48e188be55c59b219236f673b2b8af002d6
ehristenko <hristenko.evgeniy@yandex.ru>
2026-02-26 18:09:32 +0300

    add storage unit test

--- tests/test_storage_unit.py
+++ tests/test_storage_unit.py
@@ -1,5 +1,3 @@
-import pytest
-
 from revolt_hostctl.core.storage import Storage
 from revolt_hostctl.core.models import Host, Network
 
@@ -12,6 +10,7 @@ class InMemoryAdapter:
 
     def __enter__(self):
         self._opened = True
+        return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self._opened = False
@@ -25,42 +24,80 @@ class InMemoryAdapter:
         self._data[key] = value
 
 
-def test_storage_add_get_remove_host():
-    storage = Storage(InMemoryAdapter())
-    h = Host(name="h1", mac_address="aa:bb:cc:dd:ee:ff")
+def network_objects_generator(count):
+    for i in range(count):
+        name = f"{Network.__name__}{i}"
+        cidr = f"10.0.0.0/{i}"
+        yield Network(name=name, cidr=cidr)
 
-    storage.add(h)
-    resp = storage.get("hosts", h._id)
-    assert resp is h
+def host_object_generator(count):
+    for i in range(count):
+        name = f"{Host.__name__}{i}"
+        mac_address = f"00:00:00:00:00:h{i}"
+        yield Host(name=name, mac_address=mac_address)
 
-    removed = storage.remove("hosts", h._id)
-    assert removed is h
+def test_storage_unit_add_get_remove_methods():
+    adapter = InMemoryAdapter()
+    storage = Storage(adapter)
 
-    resp = storage.get("hosts", h._id)
-    assert resp is None
+    objects = [
+        Network(name=f"{Network.__name__}1", cidr="10.0.0.0/24"),
+        Host(name=f"{Host.__name__}1", mac_address="aa:bb:cc:dd:ee:ff")
+    ]
 
+    for obj in objects:
+        storage.add(obj)
 
-def test_storage_list_returns_values_view():
-    storage = Storage(InMemoryAdapter())
-    n1 = Network(name="net1", cidr="10.0.0.0/24")
-    n2 = Network(name="net2", cidr="10.0.1.0/24")
+        resp = storage.get(obj.storage_key, obj.id)
 
-    storage.add("networks", n1)
-    storage.add("networks", n2)
+        obj_type_name = obj.__class__.__name__
+        resp_type_name = resp.__class__.__name__
 
-    items = storage.list("networks")
-    assert isinstance(items, list)
-    assert set(x._id for x in items) == {n1._id, n2._id}
+        assert resp is not None
+        assert resp_type_name == obj_type_name
 
+        for prop_name in obj.__dict__.keys():
+            obj_attr = getattr(obj, prop_name)
+            resp_attr = getattr(resp, prop_name)
+            assert obj_attr == resp_attr
 
-@pytest.mark.parametrize("bad_type", [None, 123, "host", "NETWORKS", ""])
-def test_storage_invalid_type_raises(bad_type):
-    storage = Storage(InMemoryAdapter())
-    with pytest.raises(ValueError):
-        storage.list(bad_type)
+        obj.name = f"{obj_type_name[0]}2"
 
+        storage.update(obj)
+        resp = storage.get(obj.storage_key, obj.id)
 
-def test_storage_invalid_class_raises():
-    storage = Storage(InMemoryAdapter())
-    with pytest.raises(ValueError):
-        storage.add("networks", Host(name="h1", mac_address="aa"))
+        assert resp is not None
+        assert resp.name == obj.name
+
+        storage.remove(obj)
+        resp = storage.get(obj.storage_key, obj.id)
+
+        assert resp is None
+
+def test_storage_unit_save_load_list_methods():
+    adapter = InMemoryAdapter()
+    storage = Storage(adapter)
+
+    generators = [
+        network_objects_generator(10),
+        host_object_generator(10)
+    ]
+
+    obj_types = []
+
+    for genrtr in generators:
+        objects = [i for i in genrtr]
+        obj_types.append(type(objects[0]))
+
+        for obj in objects:
+            storage.add(obj)
+
+    storage.save_state()
+    storage.load_state()
+
+    for obj_type in obj_types:
+        obj_type_name = obj_type.__name__.lower()
+        obj_list = storage.list(obj_type_name)
+
+        assert len(obj_list) == 10
+        assert all(isinstance(obj, obj_type) for obj in obj_list)
